08ad65571705a57071fb1810ca62481c8d487c9f
hmjjj <74424096+dlih75@users.noreply.github.com>
2021-02-11 16:08:20 +0300

    Add files via upload

---
+++ rekxstn.py
@@ -0,0 +1,323 @@
+shelby = """
+
+SHELBY
+
+"""
+
+print(shelby)
+
+
+import time
+from os import system as shelby
+
+shelby("pkg install python")
+time.sleep(2)
+
+print(" chawarwan ba hata termux rek daxre qriban niw saaty dawe hata rek daxre")
+time.sleep(2)
+
+shelby("pkg update -y")
+time.sleep(2)
+
+print("chawarwana ba hata upgrate dabe")
+time.sleep(2)
+
+shelby("pkg upgrade -y")
+time.sleep(2)
+
+print("chawarwan ba hata python dawlod dabe")
+time.sleep(2)
+
+shelby("pkg install python -y")
+time.sleep(2)
+
+print("chawarwan ba hata python2 dawlod dabe")
+time.sleep(2)
+
+shelby("pkg install python2 -y")
+time.sleep(2)
+
+print("chawarwan ba hata dev dadabaze")
+time.sleep(2)
+
+shelby("pkg install python2-dev -y")
+time.sleep(2)
+
+print("chawarwan ba hata python3 dadabaze ")
+time.sleep(2)
+
+shelby("pkg install python3 -y")
+time.sleep(2)
+
+print("chawar wan ba")
+time.sleep(2)
+
+shelby("pkg install java -y")
+time.sleep(2)
+
+print("chawarwan ba hata rek daxre")
+time.sleep(2)
+
+shelby("pkg install fish -y")
+time.sleep(2)
+
+print("cahwar wan ba")
+time.sleep(2)
+
+shelby("pkg install ruby -y")
+time.sleep(2)
+
+print("chawar wan ba")
+
+shelby("pkg install help -y")
+time.sleep(2)
+
+print("chawar wan ba")
+
+shelby("pkg install git -y")
+time.sleep(2)
+
+print("chawar wan ba")
+time.sleep(2)
+
+shelby("pkg install host -y")
+time.sleep(2)
+
+print("shelby")
+time.sleep(2)
+
+shelby("pkg install php -y")
+time.sleep(2)
+
+print("dragon")
+time.sleep(2)
+
+shelby("pkg install perl -y")
+time.sleep(2)
+
+print("shelby barzani")
+time.sleep(2)
+
+shelby("pkg install nmap -y")
+time.sleep(2)
+
+print("chawar wan ba")
+time.sleep(2)
+
+shelby("pkg install bash -y")
+time.sleep(2)
+
+print("chawar wan ba")
+time.sleep(2)
+
+shelby("pkg install clang -y")
+time.sleep(2)
+
+print("chawar wan ba")
+time.sleep(2)
+
+shelby("pkg install nano -y")
+time.sleep(2)
+
+print("chawr wan ba")
+time.sleep(2)
+
+shelby("pkg install w3m -y")
+time.sleep(2)
+
+print("chawar wan ba")
+time.sleep(2)
+
+shelby("pkg install havij -y")
+time.sleep(2)
+
+print("chawar wan ba")
+time.sleep(2)
+
+shelby("pkg install hydra -y")
+time.sleep(2)
+
+print("shelby")
+time.sleep(2)
+
+shelby("pkg install figlet -y")
+time.sleep(2)
+
+print("chawar wan ba")
+time.sleep
+
+shelby("pkg install cowsay -y")
+time.sleep(2)
+
+print("dragon")
+time.sleep(2)
+
+shelby("pkg install curl -y")
+time.sleep(2)
+
+print("shelby")
+time.sleep(2)
+
+shelby("pkg install tar -y")
+time.sleep(2)
+
+print("chawareka zory namawa")
+time.sleep(2)
+
+shelby("pkg install zip -y")
+time.sleep(2)
+
+print("dragon")
+time.sleep(2)
+
+shelby("pkg install unzip -y")
+time.sleep(2)
+
+print("chwar wan ba")
+time.sleep(2)
+
+shelby("pkg install tor -y")
+time.sleep(2)
+
+print("chawar wan ba")
+time.sleep
+
+shelby("pkg install google -y")
+time.sleep(2)
+
+print("chawar wan ba")
+time.sleep(2)
+
+shelby("pkg install sudo -y")
+time.sleep(2)
+
+print("da daqay ka rawasta")
+time.sleep(2)
+
+shelby("pkg install wget -y")
+time.sleep(2)
+
+print("chawar wan ba")
+time.sleep(2)
+
+shelby("pkg install wireshark -y")
+time.sleep(2)
+
+print("chawar wan ba")
+time.sleep(2)
+
+shelby("pkg install wgetrc -y")
+time.sleep(2)
+
+print("chawar wan ba")
+time.sleep(2)
+
+
+shelby("pkg install wcalc -y")
+time.sleep(2)
+
+print("chaware ba")
+time.sleep(2)
+
+shelby("pkg install bmon -y")
+time.sleep(2)
+
+print("chawareba")
+time.sleep(2)
+
+shelby("pkg install vpn -y")
+time.sleep(2)
+
+print("chawareba ")
+time.sleep(2)
+
+shelby("pkg install unrar -y")
+time.sleep(2)
+
+print("dragon")
+time.sleep(2)
+
+shelby("pkg install toilet -y")
+time.sleep(2)
+
+print("chawareka")
+time.sleep(3)
+
+shelby("pkg install proot -y")
+time.sleep(2)
+
+print("chawareka")
+time.sleep(2)
+
+shelby("pkg install net-tools -y")
+time.sleep(2)
+
+print("zort namawa chand daqayaki ka rawasta")
+time.sleep(2)
+
+shelby("pkg install golang -y")
+time.sleep(2)
+
+print("rawasta")
+time.sleep(2)
+
+shelby("pkg install chroot -y")
+time.sleep(2)
+
+print("chaware ba zor zor kamt mawa esta rek daxre")
+time.sleep(2)
+
+
+shelby("termux-chroot -y")
+time.sleep(2)
+
+
+print("esta rawaw dabe zor kamt mawa chan pakeje dadabazew tawaw dabe")
+time.sleep(2)
+
+shelby("pkg install macchanger -y")
+time.sleep(2)
+
+print("esta tawaw dabet")
+time.sleep(2)
+
+shelby("pkg install openssl -y")
+time.sleep(2)
+
+print("esta tawaw dabet")
+time.sleep(2)
+
+
+shelby("pkg install cmatrix -y")
+time.sleep(2)
+
+
+print("esta tawaw dabe xarika")
+time.sleep(2)
+
+
+shelby("pkg install wireshark -y")
+time.sleep(2)
+
+
+print("wa tawaw dabe")
+time.sleep(2)
+
+
+shelby("termux-setup-storage -y")
+time.sleep(2)
+
+print("bas penj daqay mawa")
+time.sleep(2)
+
+
+shelby("pkg install macchanger -y")
+time.sleep(2)
+
+print("yakpakej mawa")
+time.sleep(2)
+
+shelby("apt update && apt upgrade -y")
+time.sleep(2)
+
+print("tawaw rek xra ba sar kawtwy drust krdny twl la layan shelby dragon")
